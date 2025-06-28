face1e3e364fbeced09e3c004c89bb9f3791c49d
Edvard Granheim Harbo <edvargh@stud.ntnu.no>
2025-06-28 20:27:58 +0200

    Initial commit

---
+++ src/test/java/com/roomies/RoomiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomiesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
